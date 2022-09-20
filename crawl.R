bbe0e80cf2ee7ff4efc312f54f744e871faa79b7
turbanisch <fetu@gmx.de>
2022-09-20 17:03:24 +0200

    create overview table + get_short_name
    Overview table contains English names as identifiers and links to country pages. The localized links only differ wrt. to the language identifier.
    `get_short_name` scrapes the short country name from the Wikipedia page heading.

--- crawl.R
+++ crawl.R
@@ -1,7 +1,50 @@
 library(tidyverse)
 library(rvest)
 
-# urls
+walk(fs::dir_ls("R/"), source)
+
+
+# scrape overview page ----------------------------------------------------
+
+# from simplified version
+overview_url <- "https://zh.wikipedia.org/zh-cn/世界政區索引"
+
+# scrape table nodes
+overview_tables <- overview_url %>% 
+  read_html() %>% 
+  html_elements("table") %>% 
+  # subset relevant tables
+  .[3:27] 
+
+# extract tables from table nodes
+overview <- overview_tables %>% 
+  html_table() %>% 
+  bind_rows()
+
+# extract urls to country page from table
+country_urls <- overview_tables %>% 
+  html_elements("td:nth-child(1) > a") %>% 
+  html_attr("href") %>% 
+  str_remove("^/wiki/") %>% 
+  # remove links to images
+  .[!str_detect(., "^File")]
+
+# keep only English country name and ISO code, add URL
+overview <- overview %>% 
+  # convert country codes to NA unless a full set is provided (iso2c-iso3c-iso3n)
+  mutate(ISO代码 = if_else(str_detect(ISO代码, "-.*-"), ISO代码, NA_character_)) %>% 
+  separate(ISO代码, into = c("iso2c", "iso3c", "iso3n")) %>% 
+  # drop columns
+  select(short_name_en = 英文简称, iso3c) %>% 
+  add_column(url = country_urls)
+  
+overview
+
+
+
+# rest --------------------------------------------------------------------
+
+# build urls to localized overview page
 variant <- c(
   "zh-cn", # 大陆简体
   "zh-hk", # 香港繁體
@@ -11,30 +54,6 @@ variant <- c(
   "zh-tw" # 台灣整體
 )
 
-base_url <- "https://zh.wikipedia.org"
-
-urls <- str_c(base_url, variant, "世界政區索引", sep = "/")
-
-tables <- urls[1] %>% 
-  read_html() %>% 
-  html_elements("table") %>% 
-  # subset relevant tables
-  .[3:27] 
-
-country_names <- tables %>% 
-  html_elements("td:nth-child(1) > a") %>% 
-  html_text2()
-
-country_links <- tables %>% 
-  html_elements("td:nth-child(1) > a") %>% 
-  html_attr("href") %>% 
-  str_remove("^/wiki/")
-
-df <- tibble(
-  country = country_names,
-  url = country_links 
-) %>% 
-  filter(country != "")
 
 # complete links to variant-specific site (here: simplified)
 str_c(base_url, variant[1], df$url, sep = "/")
@@ -45,3 +64,26 @@ output <- map(
   get_names
 )
 
+# get short name from page heading
+get_short_name <- function(url) {
+  url %>% 
+    read_html() %>% 
+    html_element("h1#firstHeading") %>% 
+    html_text2()
+}
+
+short_name <- map(
+  str_c(base_url, variant[1], df$url, sep = "/"),
+  get_short_name
+)
+
+full_name <- map(output, 1)
+variant <- map(output, ~.x[-1]) %>% map2(short_name, ~setdiff(.x, .y))
+
+scraped <- tibble(
+  short_name = unlist(short_name),
+  full_name = unlist(full_name),
+  variant
+)
+
+scraped %>% unnest_wider(variant)
