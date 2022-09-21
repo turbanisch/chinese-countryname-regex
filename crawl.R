b3f6a6acfda024851af2c7253cb1a3e13c633faa
turbanisch <fetu@gmx.de>
2022-09-22 00:35:12 +0200

    reformat test section

--- crawl.R
+++ crawl.R
@@ -31,16 +31,25 @@ write_csv(dict, "data/dict.csv")
 
 # test regex --------------------------------------------------------------
 
-# which ones were matched to a wrong country?
-countrynames_lcs %>% 
-  fuzzyjoin::regex_left_join(regexes, by = c("name" = "regex")) %>% 
+# `countrynames_lcs` contains all variants, converted into simplified Chinese
+matched <- countrynames_lcs %>%
+  select(short_name_en, variant) %>%
+  fuzzyjoin::regex_left_join(regexes, by = c("variant" = "regex"))
+
+# which entries could not be matched?
+matched %>%
+  filter(is.na(short_name_en.y))
+# only the ones that I removed on purpose: outdated names (布鲁克巴, 波斯, 溜山), literal translations (冰封之岛, 奥特亚罗瓦) and ambiguous ones (刚果)
+
+# check if countries were matched more than once
+matched %>%
+  janitor::get_dupes(short_name_en.x, variant)
+# none
+
+# which entries were matched to a wrong country?
+matched %>%
   filter(short_name_en.x != short_name_en.y)
 # only Taiwan's variant 中国
-
-# which ones could not be matched?
-countrynames_lcs %>% 
-  fuzzyjoin::regex_anti_join(regexes, by = c("name" = "regex"))
-# only the ones that I removed on purpose: outdated names (布鲁克巴, 波斯, 溜山), literal translations (冰封之岛, 奥特亚罗瓦) and ambiguous ones (刚果)
 
 
 
