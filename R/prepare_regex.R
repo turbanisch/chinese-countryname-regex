3c5c0741b92d0d8360c9fedd9310b988ac92c82b
turbanisch <fetu@gmx.de>
2022-09-21 23:26:02 +0200

    init
    refactor from crawl.R

---
+++ R/prepare_regex.R
@@ -0,0 +1,22 @@
+prepare_regex <- function(countrynames_wide) {
+  # convert everything to simplified and keep unique variants
+  variants <- countrynames_wide %>%
+    pivot_longer(
+      cols = short_name:last_col(),
+      names_to = "x",
+      values_to = "name"
+    ) %>%
+    filter(!is.na(name)) %>%
+    mutate(name = ropencc::converter(T2S)[name]) %>%
+    distinct(short_name_en, name)
+  
+  # add longest common substring as basis for regex
+  variants %>%
+    group_by(short_name_en) %>%
+    summarise(variant = list(c(name))) %>%
+    mutate(common_string = map_chr(variant , PTXQC::LCSn)) %>%
+    unnest_longer(variant) %>%
+    # pre-fill regex if one of the variants is a substring of all other variants
+    group_by(short_name_en) %>%
+    mutate(regex = if_else(common_string %in% variant, common_string, ""))
+}
