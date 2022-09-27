2557ff5d5cb22eca2146e9d298d033201b27cbbc
turbanisch <fetu@gmx.de>
2022-09-27 18:57:33 +0200

    import manual regexes

--- R/read_and_clean_regex.R
+++ R/read_and_clean_regex.R
@@ -1,8 +1,8 @@
 read_and_clean_regex <- function(path) {
   read_csv(path, col_types = cols(.default = col_character())) %>%
-    select(short_name_en, regex) %>%
+    select(iso3c, regex) %>%
     # first row of each country contains regex
-    group_by(short_name_en) %>%
+    group_by(iso3c) %>%
     filter(row_number() == 1L) %>%
     ungroup()
 }
