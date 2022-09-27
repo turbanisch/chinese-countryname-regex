bc79440e99041e6b535dc2453fb34864a756e9d5
turbanisch <fetu@gmx.de>
2022-09-27 13:58:40 +0200

    pre-fill regex
    Find longest common substring as before, but already add regexes that I created manually earlier

--- R/prepare_regex.R
+++ R/prepare_regex.R
@@ -8,15 +8,16 @@ prepare_regex <- function(countrynames_wide) {
     ) %>%
     filter(!is.na(name)) %>%
     mutate(name = ropencc::converter(T2S)[name]) %>%
-    distinct(short_name_en, name)
+    distinct(iso3c, name)
   
   # add longest common substring as basis for regex
   variants %>%
-    group_by(short_name_en) %>%
+    group_by(iso3c) %>%
     summarise(variant = list(c(name))) %>%
     mutate(common_string = map_chr(variant , PTXQC::LCSn)) %>%
     unnest_longer(variant) %>%
     # pre-fill regex if one of the variants is a substring of all other variants
-    group_by(short_name_en) %>%
-    mutate(regex = if_else(common_string %in% variant, common_string, ""))
+    group_by(iso3c) %>%
+    mutate(regex = if_else(common_string %in% variant, common_string, "")) %>% 
+    ungroup()
 }
